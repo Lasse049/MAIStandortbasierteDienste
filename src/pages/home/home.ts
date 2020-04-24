85c1ffc22744271343dc43d93f7ad44447531549
Lasse049 <lassehyb@gmail.com>
2020-04-24 17:57:18 +0200

    initial

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,16 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import leaflet from 'leaflet';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  @ViewChild('map') mapContainer: ElementRef;
+  map: any;
   constructor(public navCtrl: NavController) {
 
   }
 
-}
+  ionViewDidEnter() {
+    this.loadmap();
+  }
 
-//TEST
+  loadmap() {
+    this.map = leaflet.map("map").fitWorld();
+    leaflet.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attributions: 'www.tphangout.com',
+      maxZoom: 18
+    }).addTo(this.map);
+  }
+
+}
