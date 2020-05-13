51015464795022fd27f89cefe2e32ebc15336206
Lasse049 <lassehyb@gmail.com>
2020-05-13 18:35:43 +0200

    LÄUFT Comments added Bluedot Bugfixes Networkcheck added 5.Fix Filter added... (abfangen fehlt) Nav Button added... (style 404) 1.improve Filterbutton Added 1.improve Errorhandling added
    PORT GEÄNDERT!! SERVERJS.22 !!!

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -498,11 +498,10 @@ export class HomePage {
           } else if (this.jsondata[i].sondermuell == true) {
             this.marker.bindPopup('<br>' + this.jsondata[i].time + ' <br> Username: ' + this.jsondata[i].username + '<br>' + ' Sondermuell');
           }
-          this.bluedot = null;
-          this.showBlueDot();
           this.markers.addLayer(this.marker);
           console.log("Markers added");
         }
+        this.showBlueDot();
       } else {
         this.setFilterMarker();
       }
@@ -574,7 +573,6 @@ export class HomePage {
         this.fmarkers.addLayer(this.sondermuell);
       }
     }
-    this.bluedot = null;
     this.showBlueDot();
     console.log("ANDHERE");
     this.fmarkers.addTo(this.map);
