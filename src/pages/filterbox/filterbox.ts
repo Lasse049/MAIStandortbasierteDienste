316f22ad04bdec45d25f37137a57f80cd253c900
Czso23 <fcziudaisonn@uos.de>
2020-05-16 17:24:02 +0200

    Kommentare

--- src/pages/filterbox/filterbox.ts
+++ src/pages/filterbox/filterbox.ts
@@ -30,11 +30,7 @@ export class FilterboxPage {
   data: any; //Data of Server got from homepage
   data2: any = [];
   hausmuellarr: any = []; // Array for only Hausmülldata
-  sperrmuellarr: any = []; // Array for only Sperrmüll
-  sondermuellarr: any = [];// Array for only Sondermüll
-  guenabfallarr: any = []; // Array for only Grünabfall
   namearr: any = []; // Array to filter Data by username oder date
-  datearr: any = [];
   filterboolean: any = false;// boolean for recognising if there is some filter working
   originialdata:any; // orginaldata from the server
   b:boolean
@@ -76,60 +72,59 @@ export class FilterboxPage {
   }
 
   filter2() {                                             //Filterfunktion - started when Button is clicked
-    // braucht die nächste zeile wirklich?
-    //this.data = JSON.parse(JSON.stringify(this.data));
-    console.log('button works!!')
-    if(this.finput != undefined){
-      if(this.finput[0] == null){
-        this.finput = null
+
+    console.log('button works!!')  //Consolelog printed when button is clicked and method starts
+    if(this.finput != undefined){  // if finput (set in the Filterbox.html) is not undefined/ when there is/was an input..
+      if(this.finput[0] == null){  // ...and If there is no entry in finput... (when text is deleted)...
+        this.finput = null         // ...finput is set null
       }
     }
     console.log(this.finput)
-    //if(this.finput!=null|| ){
+
     if (this.data != null) {                             // Only works when there are data from the Server
       let g = 0;                                         // counting Variable
       console.log(this.finput)
       if(this.finput != null && this.fdate != null){     // if there is an input for username and date
-        for (let a = 0; a < this.data.length; a++) {
-          var dateTime = this.data[a].time;
-          var getdateTime = dateTime.split("T",1);
-          if (this.data[a].username == this.finput && getdateTime == this.fdate) {
-            this.namearr[g] = this.data[a];
-            g++;
+        for (let a = 0; a < this.data.length; a++) {     // for every entry of the dataset ...
+          var dateTime = this.data[a].time;              //... extract date and time from the entry
+          var getdateTime = dateTime.split("T",1); // and split the Date and the Time/ Date is saved in getdateTime
+          if (this.data[a].username == this.finput && getdateTime == this.fdate) { //if the name and the date of the Datasetentry is equal to the entry in the Filterboxpage...
+            this.namearr[g] = this.data[a];              // the datasetentry is written in the namearray
+            g++                                          // the index of the namearray is increased
           }
         }
-      } else {
-        if (this.finput != null) {
-          for (let a = 0; a < this.data.length; a++) {
-            if (this.data[a].username == this.finput) {
-              this.namearr[g] = this.data[a];
-              g++;
+      } else {                                           // if there is only a entry for a name or date on the Filterboxpage
+        if (this.finput != null) {                       // if there is a entry in finput
+          for (let a = 0; a < this.data.length; a++) {   // for every entry of the dataset ...
+            if (this.data[a].username == this.finput) {  // if the nameentry of the dataset is equal to the entry given on the Filterboxpage
+              this.namearr[g] = this.data[a];            // the datasetentry is written in the namearray
+              g++;                                       // the index of the namearray is increased
             }
           }
-        } else {
-          if (this.fdate != null) {
-            for (let a = 0; a < this.data.length; a++) {
-              var dateTime = this.data[a].time;
-              var getdateTime = dateTime.split("T",1);
-              if (getdateTime == this.fdate) {
-                this.namearr[g] = this.data[a];
-                g++;
+        } else {                                         // if there is no entry for a name on the Filterboxpage
+          if (this.fdate != null) {                      // if there is a entry for a date on the Filterboxpage
+            for (let a = 0; a < this.data.length; a++) { // for every entry of the dataset ...
+              var dateTime = this.data[a].time;          // extract date and time from the entry
+              var getdateTime = dateTime.split("T",1); // and split the Date and the Time / Date is saved in getdateTime
+              if (getdateTime == this.fdate) {           // if the getdateTime of the dataset is equal to the entry given on the Filterboxpage
+                this.namearr[g] = this.data[a];          // the datasetentry is written in the namearray
+                g++;                                     // the index of the namearray is increased
               }
             }
           }
         }
       }
-      if (this.fdate == null && this.finput == null){
-        for (let a = 0; a < this.data.length; a++) {
-          this.namearr[g] = this.data[a];
-          g++;
+      if (this.fdate == null && this.finput == null){   // if there is no entry for a name or a date on the Filterboxpage
+        for (let a = 0; a < this.data.length; a++) {    // for every entry of the dataset
+          this.namearr[g] = this.data[a];               // the datasetentry is written in the namearray
+          g++;                                          // the index of the namearray is increased
         }
       }
       console.log(this.namearr)
       console.log(this.fdate)
 
-      console.log(this.datearr)
 
+// if a date and or a name is given on the Filterboxpage but there is no Type of Trash chosen all Types are set true
       if(((this.fdate != null ||this.finput != null)||(this.finput != undefined || this.fdate != undefined)) && (this.fSperrmuell == undefined || this.fSperrmuell == false) && (this.fHausmuell == undefined || this.fHausmuell == false) && (this.fGruenabfall == undefined || this.fGruenabfall == false) && (this.fSperrmuell == undefined|| this.fSperrmuell == false)){
         this.fHausmuell = true;
         this.fSperrmuell = true;
@@ -137,8 +132,9 @@ export class FilterboxPage {
         this.fSondermuell  = true;
 
       }
-      // NameArry Hausmuell was?
-      //this.namearr = JSON.parse(JSON.stringify(this.namearr));
+      /*  For every entry of the Dataset the status of the boolean of the trashtypes is checked.
+          if one of the on the Filterboxpage selected trashtypes in the datasetentry is true the data is pushed in the hausmuelarr
+      */
        for (let i = 0; i < this.namearr.length; i++) {
          if (this.namearr[i].hausmuell== true && this.fHausmuell == true && this.fHausmuell != undefined) {
            this.hausmuellarr.push(this.namearr[i]);
@@ -148,63 +144,53 @@ export class FilterboxPage {
          if (this.namearr[i].gruenabfall == true && this.fGruenabfall == true && this.fGruenabfall != undefined) {
            this.hausmuellarr.push(this.namearr[i]);
            console.log("gruen");
-           console.log(this.guenabfallarr);
 
          } else
          if (this.namearr[i].sondermuell == true && this.fSondermuell == true && this.fSondermuell != undefined) {
            this.hausmuellarr.push(this.namearr[i]);
            console.log("sonder");
-           console.log(this.sondermuellarr);
 
          } else
          if (this.namearr[i].sperrmuell == true && this.fSperrmuell == true && this.fSperrmuell != undefined) {
            this.hausmuellarr.push(this.namearr[i]);
            console.log("sper");
-           console.log(this.sperrmuellarr);
-
          };
        }
     }
-    // fitlerbool nur true wenn auch gefilter wurde und daten nicht null/leer sind!!
-    // if ((this.hausmuellarr == undefined ||this.hausmuellarr == null)&&(this.guenabfallarr == undefined ||this.guenabfallarr == null)&&(this.sondermuellarr == undefined ||this.sondermuellarr == null)&&(this.sperrmuellarr == undefined ||this.sperrmuellarr == null)){
-    if(this.hausmuellarr.length== 0 && this.guenabfallarr.length == 0 && this.sondermuellarr.length == 0 && this.sperrmuellarr.length == 0) {
+  // if there are no data pushed into the hausmuellarr because there is no dataentry of the dataset that equals the set filter
+  // the filterboolean is set false
+  // if there is data in the hausmuellarr the filterboolean is set true
+    if(this.hausmuellarr.length== 0) {
     this.filterboolean=false;
     } else {
       this.filterboolean = true;
 
     }
-
+    // combine data to the var filterdata
     let filterdata = {
       hausmuellarr:this.hausmuellarr,
-      sperrmuelarr:this.sperrmuellarr,
-      gruenabfallarr:this.guenabfallarr,
-      sondermuellarr:this.sondermuellarr,
       filterbool: this.filterboolean,
       origindata: this.originialdata
     };
-
+// go back to the Homepage and send filtered data to it
     this.navCtrl.pop().then(() => {
       // Trigger custom event and pass data to be send back
       this.events.publish('custom-user-events', filterdata);
     });
     }
 
-
+// Function to go back to the Homepage and als send the original data to it
     back(){
-      //this.filterboolean=false;
       let filterdata = {
         origindata: this.originialdata,
-        //filterbool: this.filterboolean
+
       }
       ;
       this.navCtrl.pop().then(() => {
         // Trigger custom event and pass data to be send back
         this.events.publish('custom-user-events', filterdata);
-        //this.navCtrl.pop();
       });
     }
 
 }
 
-//var dateTime = this.data[i].time;
-//var getdateTime = dateTime.split("T",1);
